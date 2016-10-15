f5456aa8be2a1b31e68ef41b62ea84bb5c984ee5
ThatThomBee <vagabondjuggle@gmail.com>
2016-10-15 19:07:44 +0100

    Update PygLatin.py

--- PygLatin.py
+++ PygLatin.py
@@ -1,11 +1,14 @@
 pyg = 'ay'
 
 original = raw_input('Enter a word:')
-
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    new_word = word [1:len(new_word)] + first + pyg 
+    new_word = word [1:len(word)] + first + pyg 
     print new_word
+elif len(original) == 0:
+    print 'You have not entered a word!'
+elif len(original) > 0 and original.isdigit():
+    print 'Real words are not numbers! Try again.'
 else:
-    print 'empty'
+    print 'Try again!'
