689c5a3f89e5b5ebe614a962b0e313ef7d88ff44
ThatThomBee <thomas.butterworth@ntlworld.com>
2016-12-29 23:22:31 +0000

    now changes goAgain to lower

--- dice_simulator.py
+++ dice_simulator.py
@@ -3,9 +3,15 @@ import random
 var = 1
 
 while var == 1:
+    dice_face = input("""
+    How many sides would you like your dice to have?
+    You can use any number you want!
+    """)
+    #This was originally just a 6 sided sim, but added dice_face to create a more customisable sim.
     input('Press enter to roll the dice...')
-    print (random.randint(1,6))
+    print (random.randint(1,int(dice_face)))
     goAgain = input('Would you like to roll again? Y/N...')
-    if goAgain[0] == 'N':
-        var = 2
+    goAgain = goAgain.lower()
+    if goAgain[0] == 'n':
+        var = 0
         print('Goodbye')
