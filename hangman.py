8ca4a36099e10bcc33dc68ac3610732274d625df
ThatThomBee <thomas.butterworth@ntlworld.com>
2016-12-30 02:11:49 +0000

    Adding more on

--- hangman.py
+++ hangman.py
@@ -22,5 +22,7 @@ while goes_left != 0:
     print (g_word)
     guess_letter = input("Try to guess a letter!")
     guess_letter = guess_letter.lower()
+    if len(guess_letter) != 1 or guess not in "abcdefghijklmnopqrstuvwxyz":
+        print("That is not a letter, try again.")
 
     goes_left = goes_left -1
